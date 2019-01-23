e20fd98801a8c758a35e5abebb2fd2a9843f19c4
kumar.nani25@gmail.com <naveen.nani25@gmail.com>
2019-01-23 08:53:20 +0530

    commited in loccal branch

---
+++ javaproject/src/main/java/javaprograms/swapoftwonumber.java
@@ -0,0 +1,18 @@
+package javaprograms;
+
+public class swapoftwonumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10,b=20,temp;;
+		
+		temp=a;
+		a=b;
+		b=temp;
+		
+		System.out.println("After swaping the values are "+a+"&"+b);
+
+	}
+
+}
