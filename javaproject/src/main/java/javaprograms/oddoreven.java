c263e170243a8e0655db9cbca9de6aef14bc514b
Naveen Swetha <naveen.nani25@gmail.com>
2019-01-23 08:12:39 +0530

    commit by owner

---
+++ javaproject/src/main/java/javaprograms/oddoreven.java
@@ -0,0 +1,21 @@
+package javaprograms;
+
+public class oddoreven {
+
+	public static void main(String[] args) {
+		
+		
+		for(int i=1;i<=10;i++)
+		{
+			if(i%2==0)
+			
+				System.out.println(i+ " "+ "is Even Number");
+			else
+				
+				System.out.println(i+ " "+ "is odd Number");
+			
+		}
+
+	}
+
+}
